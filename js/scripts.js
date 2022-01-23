2b80905a23df69c5dc5421dfcd66f4dc76e2362e
MareikeHavemann <tipptipptapp@yahoo.de>
2022-01-23 22:45:54 +0100

    created var 'pokemonList'
    Added four Pokemon with three values each.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
-alert('Hello world');
-
-let favouriteFood = 'Pizza';
-document.write(favouriteFood);
+let pokemonList = [
+  {
+    name: "Bulbasaur",
+    height: 2.04,
+    type: ['grass','poison']
+  },
+  {
+    name: "Ivysaur",
+    height: 3.03,
+    type: ['grass','poison']
+  },
+  {
+    name: "Venusaur",
+    height: 6.07,
+    type: ['grass','poison']
+  },
+  {
+    name: "Charmander",
+    height: 2.00,
+    type: 'fire'
+  }
+];
