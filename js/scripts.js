b25dfd5254e3a6e4a67a77b345a7c70e1e09a6b3
MareikeHavemann <tipptipptapp@yahoo.de>
2022-01-28 01:39:10 +0100

    added conditional

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,30 @@ let pokemonList = [
   {
     name: "Bulbasaur",
     height: 2.04,
-    type: ['grass','poison']
+    type: ["grass","poison"]
   },
   {
     name: "Ivysaur",
     height: 3.03,
-    type: ['grass','poison']
+    type: ["grass","poison"]
   },
   {
     name: "Venusaur",
     height: 6.07,
-    type: ['grass','poison']
+    type: ["grass","poison"]
   },
   {
     name: "Charmander",
     height: 2.00,
-    type: 'fire'
+    type: "fire"
   }
 ];
+
+// Conditional checks if height > 5
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 5) {
+    document.write(pokemonList[i].name + ( ', height: ' ) + pokemonList[i].height + " - Wow, that\'s big!" + '<br>');  }
+    else {
+      document.write(pokemonList[i].name + ( ', height: ' ) + pokemonList[i].height + '<br>');
+    }
+  }
